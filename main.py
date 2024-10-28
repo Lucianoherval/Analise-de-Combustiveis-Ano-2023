53704e90af770d0c9f386d774f058c97aef098ad
Luciano Herval <lucianoherval@gmail.com>
2024-10-28 14:05:24 -0300

    atualização

--- main.py
+++ main.py
@@ -32,17 +32,21 @@ dados.fillna(0)
 st.subheader('**Uma visão detalhada sobre as variações de preços e consumo de combustíveis ao longo do ano de 2023**')
 st.header('', divider='rainbow')
 
+# CRIANDO COLUNAS
 col1, col2 = st.columns(2)
 col3 = st.columns(1)[0]
 col4, col5 = st.columns(2)
+col6, col7 = st.columns(2)
 
+# SELEÇÃO DO COMBUSTÍVEL
 tipo_combus = dados['Produto'].unique()
 selecao_combust = st.sidebar.selectbox('Selecione o Combustível', tipo_combus)
 
-
+# MAIOR E MENOR PREÇO
 menor_preco = dados.loc[dados['Valor de Venda'].idxmin()]
 maior_preco = dados.loc[dados['Valor de Venda'].idxmax()]
 
+# MAIOR E MENOR PREÇO NA COLUNA COMO DESCRIÇÃO
 with col1:
     st.write('Maior Preço Registrado:', maior_preco['Data da Coleta'],'R$:',maior_preco['Valor de Venda'],'-', maior_preco['Municipio'],'-',
               maior_preco['Estado - Sigla'], '-', maior_preco['Produto'])
@@ -50,7 +54,8 @@ with col1:
 with col2:
     st.write('Menor Preço Registrado:', menor_preco['Data da Coleta'],'R$:',menor_preco['Valor de Venda'], '-', menor_preco['Municipio'],'-',
               menor_preco['Estado - Sigla'], '-', menor_preco['Produto'])
-    
+
+# APRESENTAÇÃO DO GRÁFICO
 with col3:
     st.write('Grafico')
     dados['MES'] = dados['Data da Coleta'].apply(lambda x: str(x.year) + '-' + str(x.month))
@@ -59,7 +64,8 @@ with col3:
     # Criar o gráfico
     fig = px.bar(filtro, x='Estado - Sigla', y='Valor de Venda', title="Gráfico de Preços por Estado")
     st.plotly_chart(fig)
-    
+
+# MÉDIA DE PREÇO POR MUNICÍPIO E TIPO DE COMBUSTÍVEL    
 with col4:
     st.subheader('Média de Preço por Municipio e Tipo de Combustível')
 
@@ -68,6 +74,7 @@ with col4:
     st.write(preco_combust_selecionado)
     print(preco_combust_selecionado.columns)
 
+# MÉDIA DE PREÇO POR ESTADO E TIPO DE COMBUSTÍVEL
 with col5:
     st.subheader('Média de Preço por estado e tido de combustível')
     preco_combust_estado = dados[dados['Produto'] == selecao_combust].groupby(['Estado - Sigla','Bandeira'])['Valor de Venda'].mean()
@@ -75,6 +82,52 @@ with col5:
     st.write(preco_combust_estado)
     print(preco_combust_estado)
 
-def nada():
-    pass
+# CONCLUSÕES
+with col6:
+
+    st.header('Conclusões')
+    st.write('A análise dos preços de combustíveis em 2023 revelou que:')
+    st.write('* O maior preço registrado foi de R$ {:.2f} em {}'.format(maior_preco['Valor de Venda'], maior_preco['Municipio']))
+    st.write('* O menor preço registrado foi de R$ {:.2f} em {}'.format(menor_preco['Valor de Venda'], menor_preco['Municipio']))
+    st.write('* A média de preço por estado é:')
+    st.write(preco_combust_estado)
+
+# RECOMENDAÇÕES
+with col7:
+        
+    st.header('Recomendações')
+    st.write('Com base na análise, recomendamos:')
+    st.write('* Monitorar os preços de combustíveis em diferentes estados e municípios')
+    st.write('* Avaliar a relação entre os preços de combustíveis e a economia local')
+    st.write('* Desenvolver estratégias para reduzir os custos de combustíveis')
+
+#RODAPÉ
+footer="""<style>
+a:link , a:visited{
+color: green;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: right;
+}
+</style>
+<div class="footer">
+Developed with 📊 by <a style='display: block; text-align: right;' href="https://www.linkedin.com/in/lucianoherval/" target="_blank">Luciano Herval S. L. Filho</a>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
 
